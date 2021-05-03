745431ecc14295154d2372bdbecd1c1392d502b2
SANTIAGO_PALACIOS <s.p.u.san.palacios@gmail.com>
2021-05-03 07:24:47 -0500

    Agregamos el login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,9 +37,30 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDTO.Username);
+
+            if(user == null) return Unauthorized("Usuario no existe");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Password invalido");
+            }
+
+            return user;
+            
+        }
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(variable => variable.UserName == username.ToLower());
         }
+
+        
     }
 }
